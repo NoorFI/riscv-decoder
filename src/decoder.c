30146f25003f3963ca77020ceadddcdaa6e4c893
NoorFI <noorfatimaihsan@gmail.com>
2026-06-29 14:46:47 +0500

    Implement extract_field and sign_extend utilities

--- src/decoder.c
+++ src/decoder.c
@@ -1,9 +1,18 @@
 #include "decoder.h"
-//initializing functions, using prototypes defined
+
 uint32_t extract_field(uint32_t instruction, int high, int low){
-    return 0;
+    //Parameters: A 32 bit instruction, high and low bits.
+    //Returns the extracted bits
+    int width = (high - low) + 1;
+    uint32_t mask = (1U << width) - 1;
+    return (instruction >> low) & mask;
 }
+
 int32_t sign_extend(uint32_t value, int bits){
-    return 0;
+    //Parameters: A 32 bit unsigned value and number of bits
+    //Returns 32 bit signed integer
+    uint32_t mask = 1U << (bits - 1);
+    return (value ^ mask) - mask;
 }
+
 void decode_instruction(uint32_t instruction, decoded_instr_t *decoded){}
--- src/main.c
+++ src/main.c
@@ -3,22 +3,20 @@
 #include "memory.h"
 
 int main(int argc, char *argv[]){ //For command line arguments
-    if(argc != 2){
-        printf("Usage:\n");
-        printf("%s <hex_file>\n",argv[0]);
-        return 1;
-    }
-
-    uint8_t memory[MEMORY_SIZE] = {0};
-    int instructions = load_hex_file(argv[1], memory);
+    uint32_t instruction = 0x00A28233;
     
-    if(instructions < 0){
-        return 1;
-    }
-    
-    printf("Instructions Loaded: %d\n", instructions);
+    printf("Instruction = 0x%08X\n", instruction);
+    printf("\n");
+    printf("opcode  = %u\n", extract_field(instruction, 6, 0));
+    printf("rd      = %u\n", extract_field(instruction, 11, 7));
+    printf("func3   = %u\n", extract_field(instruction, 14, 12));
+    printf("rs1     = %u\n", extract_field(instruction, 19, 15));
+    printf("rs2     = %u\n", extract_field(instruction, 24, 20));
+    printf("func7   = %u\n", extract_field(instruction, 31, 25));
     printf("\n");
     
-    memory_dump(memory, instructions * 4);
+    printf("Sign extend test:\n");
+    printf("%d\n", sign_extend(0xFFF, 12));
+    
     return 0;
 }
