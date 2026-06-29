739c5f13c2b0280c4f2673603b768acd75759e61
NoorFI <noorfatimaihsan@gmail.com>
2026-06-29 14:09:09 +0500

    Implement hex file loading and memroy dump

--- src/main.c
+++ src/main.c
@@ -3,6 +3,22 @@
 #include "memory.h"
 
 int main(int argc, char *argv[]){ //For command line arguments
-    printf("RISC-V RV32I Instruction Decoder\n"); //testing if it compiles
+    if(argc != 2){
+        printf("Usage:\n");
+        printf("%s <hex_file>\n",argv[0]);
+        return 1;
+    }
+
+    uint8_t memory[MEMORY_SIZE] = {0};
+    int instructions = load_hex_file(argv[1], memory);
+    
+    if(instructions < 0){
+        return 1;
+    }
+    
+    printf("Instructions Loaded: %d\n", instructions);
+    printf("\n");
+    
+    memory_dump(memory, instructions * 4);
     return 0;
 }
--- src/memory.c
+++ src/memory.c
@@ -1,6 +1,50 @@
 #include "memory.h"
-//Empty implementation, again just to check compilation and satisfy linker requirements
+
 int load_hex_file(const char *filename, uint8_t *memory){
-    return 0;
+    //Parameters: filename is the path to the hex file, memory is pointing to the beginning of simulated memory.
+    //It returns no. of instructions loaded.
+
+    FILE *fp = fopen(filename, "r"); //file pointer
+
+    if (fp == NULL) {
+        perror("Failed to open file");
+        return -1;
+    }
+
+    uint32_t instruction; //current instruction
+    size_t address = 0; //address inside memory
+    int count = 0; //no. of instr loaded (to be returned)
+
+    while(fscanf(fp, "%x", &instruction) == 1){
+        memory[address] = instruction & 0xFF;
+
+        for(size_t i = 1; i < 4; i++){
+            memory[address + i] = (instruction >> (8*i)) & 0xFF;
+        }
+
+        address +=4;
+        count++;
+    }
+
+    fclose(fp);
+    return count;
 }
-void memory_dump(const uint8_t *memory, size_t size){}
+void memory_dump(const uint8_t *memory, size_t size){
+    //Parameters: size is the number of bytes to be displayed.
+
+    for (size_t i = 0; i < size; i++){
+        if(i % 16 == 0){
+            printf("%04zX: ", i); //Print address every 16 bits
+        }
+        
+        printf("%02X ", memory[i]); //Print current byte
+
+        if ((i + 1) % 16 == 0){
+            printf("\n"); //After every 16 bits print new line
+        }
+    }
+
+    if (size % 16 != 0){
+        printf("\n");
+    }
+}
