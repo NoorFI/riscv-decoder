a682e54725ed5f7aff59d4bdf8e9dfd1dd8366c9
NoorFI <noorfatimaihsan@gmail.com>
2026-06-29 15:26:27 +0500

    Add enums macros and shared constants

--- include/common.h
+++ include/common.h
@@ -6,4 +6,12 @@
 #include <stdlib.h>
 #include <string.h> //All headers
 
+#define XLEN 32
+#define MEMORY_SIZE (64 * 1024)
+#define INSTRUCTION_SIZE 4
+#define MNEMONIC_SIZE 16
+#define DUMP_WIDTH 16
+#define EXTRACT_BITS(value, high, low) \
+(((value) >> (low)) & ((1U << (((high)-(low))+1)) - 1))
+
 #endif
--- include/decoder.h
+++ include/decoder.h
@@ -3,6 +3,31 @@
 
 #include "common.h"
 
+typedef enum{
+    OP_LOAD = 0x03,
+    OP_IMM = 0x13,
+    OP_AUIPC = 0x17,
+    OP_STORE = 0x23,
+    OP_REG = 0x33,
+    OP_LUI = 0x37,
+    OP_BRANCH = 0x63,
+    OP_JALR = 0x67,
+    OP_JAL = 0x6F,
+} opcode_t;
+
+typedef enum{
+    ALU_ADD,
+    ALU_SUB,
+    ALU_AND,
+    ALU_OR,
+    ALU_XOR,
+    ALU_SLL,
+    ALU_SRL,
+    ALU_SRA,
+    ALU_SLT,
+    ALU_SLTU,
+} alu_op_t;
+
 typedef struct{
     uint32_t raw;
     uint32_t opcode;
@@ -12,7 +37,7 @@ typedef struct{
     uint32_t rs2;
     uint32_t funct7;
     int32_t imm;
-    char mnemonic[16];
+    char mnemonic[MNEMONIC_SIZE];
 } decoded_instr_t;
 
 uint32_t extract_field(uint32_t instruction, int high, int low);
